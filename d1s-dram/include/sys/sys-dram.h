0f91a8a00c8ce9ed23ff1dda451b2164f4d1fbe8
YuzukiTsuru <gloomyghost@gloomyghost.com>
2023-01-01 09:42:09 +0800

    add dram

---
+++ d1s-dram/include/sys/sys-dram.h
@@ -0,0 +1,39 @@
+#ifndef __SYS_DRAM_H__
+#define __SYS_DRAM_H__
+
+#include <types.h>
+
+typedef struct dram_param {
+    uint32_t dram_clk;
+    uint32_t dram_type;
+    uint32_t dram_zq;
+    uint32_t dram_odt_en;
+    uint32_t dram_para1;
+    uint32_t dram_para2;
+    uint32_t dram_mr0;
+    uint32_t dram_mr1;
+    uint32_t dram_mr2;
+    uint32_t dram_mr3;
+    uint32_t dram_tpr0;
+    uint32_t dram_tpr1;
+    uint32_t dram_tpr2;
+    uint32_t dram_tpr3;
+    uint32_t dram_tpr4;
+    uint32_t dram_tpr5;
+    uint32_t dram_tpr6;
+    uint32_t dram_tpr7;
+    uint32_t dram_tpr8;
+    uint32_t dram_tpr9;
+    uint32_t dram_tpr10;
+    uint32_t dram_tpr11;
+    uint32_t dram_tpr12;
+    uint32_t dram_tpr13;
+    uint32_t reserve[8];
+} dram_param_t;
+
+/*
+ * Init System UART
+ */
+void sys_dram_init();
+
+#endif // __SYS_DRAM_H__
---
+++ d1s-dram/src/sys/sys-dram.c
@@ -0,0 +1,39 @@
+#include <byteorder.h>
+#include <endian.h>
+#include <io.h>
+#include <stdarg.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <string.h>
+#include <types.h>
+#include <sys-dram.h>
+#include <dram.h>
+
+static const struct dram_param param = {
+    .dram_clk = 528,
+    .dram_type = 2,
+    .dram_zq = 0x07b7bf9,
+    .dram_odt_en = 0x00,
+    .dram_para1 = 0x000000d2,
+    .dram_para2 = 0x00000000,
+    .dram_mr0 = 0x00000e73,
+    .dram_mr1 = 0x02,
+    .dram_mr2 = 0x0,
+    .dram_mr3 = 0x0,
+    .dram_tpr0 = 0x00471992,
+    .dram_tpr1 = 0x0131a10c,
+    .dram_tpr2 = 0x00057041,
+    .dram_tpr3 = 0xb4787896,
+    .dram_tpr4 = 0x0,
+    .dram_tpr5 = 0x48484848,
+    .dram_tpr6 = 0x48,
+    .dram_tpr7 = 0x1621121e,
+    .dram_tpr8 = 0x0,
+    .dram_tpr9 = 0x0,
+    .dram_tpr10 = 0x00000000,
+    .dram_tpr11 = 0x00030010,
+    .dram_tpr12 = 0x00000035,
+    .dram_tpr13 = 0x34000000,
+};
+
+void sys_dram_init() { init_DRAM(0, param); }
