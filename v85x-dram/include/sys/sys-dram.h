584597246111570ffecf7d445684d578fbb3033a
YuzukiTsuru <gloomyghost@gloomyghost.com>
2023-02-23 16:55:58 +0800

    add files

---
+++ v85x-dram/include/sys/sys-dram.h
@@ -0,0 +1,59 @@
+#ifndef __SYS_DRAM_H__
+#define __SYS_DRAM_H__
+
+#include <byteorder.h>
+#include <config.h>
+#include <endian.h>
+#include <io.h>
+#include <stdarg.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <string.h>
+#include <sys-clock.h>
+#include <types.h>
+
+typedef struct dram_param {
+  uint32_t dram_clk;
+  uint32_t dram_type;
+  uint32_t dram_zq;
+  uint32_t dram_odt_en;
+  uint32_t dram_para1;
+  uint32_t dram_para2;
+  uint32_t dram_mr0;
+  uint32_t dram_mr1;
+  uint32_t dram_mr2;
+  uint32_t dram_mr3;
+  uint32_t dram_tpr0;
+  uint32_t dram_tpr1;
+  uint32_t dram_tpr2;
+  uint32_t dram_tpr3;
+  uint32_t dram_tpr4;
+  uint32_t dram_tpr5;
+  uint32_t dram_tpr6;
+  uint32_t dram_tpr7;
+  uint32_t dram_tpr8;
+  uint32_t dram_tpr9;
+  uint32_t dram_tpr10;
+  uint32_t dram_tpr11;
+  uint32_t dram_tpr12;
+  uint32_t dram_tpr13;
+  uint32_t reserve[8];
+} dram_param_t;
+
+enum sunxi_dram_type {
+  SUNXI_DRAM_TYPE_DDR2 = 2,
+  SUNXI_DRAM_TYPE_DDR3 = 3,
+  SUNXI_DRAM_TYPE_LPDDR2 = 6,
+  SUNXI_DRAM_TYPE_LPDDR3 = 7,
+};
+
+#define SYS_DRAM_BASE_ADDR 0x40000000
+
+/*
+ * Init System UART
+ */
+void sys_dram_init();
+
+int init_DRAM(int type, dram_param_t *para);
+
+#endif // __SYS_DRAM_H__
---
+++ v85x-dram/include/sys/sys-uart.h
@@ -0,0 +1,21 @@
+#ifndef __SYS_UART_H__
+#define __SYS_UART_H__
+
+#include <byteorder.h>
+#include <config.h>
+#include <endian.h>
+#include <io.h>
+#include <stdarg.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <string.h>
+#include <types.h>
+
+/*
+ * Init System UART
+ */
+void sys_uart_init();
+
+int sys_uart_printf(const char * fmt, ...);
+
+#endif // __SYS_UART_H__
